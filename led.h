57b560104b2e53e2d26eec4af73b966b5a4c0ddb
Adly Abdullah <slebetman@yahoo.com>
2011-03-08 18:57:10 +0800

    Added alias ledSendHex for ledSendBcd
    since we can also use it to send hex values to the display.

--- led.h
+++ led.h
@@ -8,3 +8,4 @@ extern void ledSendBcd(unsigned char data);
 extern void addBcd(char* hi, char* lo, const char datahi, const char datalo);
 extern unsigned char char2bcd(unsigned char data);
 
+#define ledSendHex(x) ledSendBcd(x)
