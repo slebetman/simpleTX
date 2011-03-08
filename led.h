93940003155b315e07e4ad63a08f503beb5544ef
Adly Abdullah <slebetman@yahoo.com>
2011-03-08 17:45:52 +0800

    Added header file for 7 segment led library

---
+++ led.h
@@ -0,0 +1,10 @@
+#include "ledfont.h"
+
+//########################################################################
+// PROTOTYPES FOR 7 SEGMENT LED FUNCTIONS
+//########################################################################
+extern void ledSend(unsigned char data);
+extern void ledSendBcd(unsigned char data);
+extern void addBcd(char* hi, char* lo, const char datahi, const char datalo);
+extern unsigned char char2bcd(unsigned char data);
+
