ce800c81bd0caa05f3807e33a4aabdfb704352e3
Adly Abdullah <slebetman@yahoo.com>
2011-03-11 11:03:30 +0800

    Implemented simple 1:1 V-tail mix;

--- mix.c
+++ mix.c
@@ -3,10 +3,18 @@
 
 void mix(void)
 {
-	// simple copy mix as template:
+	int channel1_normalized, channel2_normalized, left_servo, right_servo;
+
+	output_pulse[CHANNEL3] = input_pulse[CHANNEL3];
+	output_pulse[CHANNEL4] = input_pulse[CHANNEL4];
 	
-	output_pulse[CHANNEL1] = input_pulse[AILERON];
-	output_pulse[CHANNEL2] = input_pulse[ELEVATOR];
-	output_pulse[CHANNEL3] = input_pulse[THROTTLE];
-	output_pulse[CHANNEL4] = input_pulse[RUDDER];
+	// V-tail mix:
+	channel1_normalized = input_pulse[CHANNEL1]-SERVO_MIN;
+	channel2_normalized = input_pulse[CHANNEL2]-SERVO_MIN;
+	
+	left_servo = (channel1_normalized+channel2_normalized)/2;
+	right_servo = (channel1_normalized+(SERVO_MAX-channel2_normalized))/2;
+	
+	output_pulse[CHANNEL1] = left_servo+SERVO_MIN;
+	output_pulse[CHANNEL2] = right_servo+SERVO_MIN;
 }
