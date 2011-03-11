f4664d701144e0a00d4166c9872b0db61b920d61
Adly Abdullah <slebetman@yahoo.com>
2011-03-11 16:23:37 +0800

    Use SERVO_RANGE instead of SERVO_MAX for correct calculation
    Also clarified code by explicitly naming channel2_reverse

--- mix.c
+++ mix.c
@@ -3,17 +3,19 @@
 
 void mix(void)
 {
-	int channel1_normalized, channel2_normalized, left_servo, right_servo;
+	int channel1_normalized, channel2_normalized, channel2_reverse,
+		left_servo, right_servo;
 
 	output_pulse[CHANNEL3] = input_pulse[CHANNEL3];
 	output_pulse[CHANNEL4] = input_pulse[CHANNEL4];
 	
 	// V-tail mix:
-	channel1_normalized = input_pulse[CHANNEL1]-SERVO_MIN;
-	channel2_normalized = input_pulse[CHANNEL2]-SERVO_MIN;
+	channel1_normalized = input_pulse[0]-SERVO_MIN;
+	channel2_normalized = input_pulse[1]-SERVO_MIN;
+	channel2_reverse = SERVO_RANGE-channel2_normalized;
 	
 	left_servo = (channel1_normalized+channel2_normalized)/2;
-	right_servo = (channel1_normalized+(SERVO_MAX-channel2_normalized))/2;
+	right_servo = (channel1_normalized+channel2_reverse)/2;
 	
 	output_pulse[CHANNEL1] = left_servo+SERVO_MIN;
 	output_pulse[CHANNEL2] = right_servo+SERVO_MIN;
