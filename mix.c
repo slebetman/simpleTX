6e4ada3ab485c74ff7c3e2a85315f3912c6bbf7e
Adly Abdullah <slebetman@yahoo.com>
2011-03-11 17:37:29 +0800

    Fixed syntax error

--- mix.c
+++ mix.c
@@ -10,13 +10,13 @@ void mix(void)
 	output_pulse[CHANNEL4] = input_pulse[CHANNEL4];
 	
 	// V-tail mix:
-	channel1_normalized = input_pulse[0]-SERVO_MIN;
-	channel2_normalized = input_pulse[1]-SERVO_MIN;
+	channel1_normalized = input_pulse[0].integer-SERVO_MIN;
+	channel2_normalized = input_pulse[1].integer-SERVO_MIN;
 	channel2_reverse = SERVO_RANGE-channel2_normalized;
 	
 	left_servo = (channel1_normalized+channel2_normalized)/2;
 	right_servo = (channel1_normalized+channel2_reverse)/2;
 	
-	output_pulse[CHANNEL1] = left_servo+SERVO_MIN;
-	output_pulse[CHANNEL2] = right_servo+SERVO_MIN;
+	output_pulse[CHANNEL1].integer = left_servo+SERVO_MIN;
+	output_pulse[CHANNEL2].integer = right_servo+SERVO_MIN;
 }
