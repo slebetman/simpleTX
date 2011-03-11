89737074e87528605c66eceadf8d61415395c15b
Adly Abdullah <slebetman@yahoo.com>
2011-03-11 22:15:31 +0800

    Mixed up channel 1 and channel 2, fixed

--- mix.c
+++ mix.c
@@ -3,19 +3,19 @@
 
 void mix(void)
 {
-	int channel1_normalized, channel2_normalized, channel2_reverse,
+	int channel1_normalized, channel2_normalized, channel1_reverse,
 		left_servo, right_servo;
 
 	output_pulse[CHANNEL3] = input_pulse[CHANNEL3];
 	output_pulse[CHANNEL4] = input_pulse[CHANNEL4];
 	
 	// V-tail mix:
-	channel1_normalized = input_pulse[0].integer-SERVO_MIN;
 	channel2_normalized = input_pulse[1].integer-SERVO_MIN;
-	channel2_reverse = SERVO_RANGE-channel2_normalized;
+	channel1_normalized = input_pulse[0].integer-SERVO_MIN;
+	channel1_reverse = SERVO_RANGE-channel1_normalized;
 	
-	left_servo = (channel1_normalized+channel2_normalized)/2;
-	right_servo = (channel1_normalized+channel2_reverse)/2;
+	left_servo = (channel2_normalized+channel1_normalized)/2;
+	right_servo = (channel2_normalized+channel1_reverse)/2;
 	
 	output_pulse[CHANNEL1].integer = left_servo+SERVO_MIN;
 	output_pulse[CHANNEL2].integer = right_servo+SERVO_MIN;
