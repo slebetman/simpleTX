9b938f20891dd7520bc7bc7f5ecc802b8dbb83c8
Adly Abdullah <slebetman@yahoo.com>
2011-03-15 07:24:50 +0800

    Use control names for channel assignment
    Makes it more self-documenting

--- mix.c
+++ mix.c
@@ -4,8 +4,8 @@ void mix(void)
 {
 	// simple copy mix as template:
 	
-	output_pulse[CHANNEL1].integer = input_pulse[CHANNEL1].integer;
-	output_pulse[CHANNEL2].integer = input_pulse[CHANNEL2].integer;
-	output_pulse[CHANNEL3].integer = input_pulse[CHANNEL3].integer;
-	output_pulse[CHANNEL4].integer = input_pulse[CHANNEL4].integer;
+	output_pulse[CHANNEL1].integer = input_pulse[AILERON].integer;
+	output_pulse[CHANNEL2].integer = input_pulse[ELEVATOR].integer;
+	output_pulse[CHANNEL3].integer = input_pulse[THROTTLE].integer;
+	output_pulse[CHANNEL4].integer = input_pulse[RUDDER].integer;
 }
