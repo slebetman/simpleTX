0f545e8257cebd75249d609327e7ff0acf6b63ae
Adly Abdullah <slebetman@yahoo.com>
2011-03-24 10:21:02 +0800

    Cleaned up main.c and removed unused debugging code

--- main.c
+++ main.c
@@ -10,13 +10,10 @@
 #include "calculations.h"
 #include "init.h"
 
-unsigned char tick;          // timer tick (roughly 1ms using 24 MHz XTAL)
+unsigned char tick; // timer tick (roughly 1ms using 24 MHz XTAL)
 
 void main(void)
 {
-	unsigned char debug_channel = 0;
-	unsigned char i;
-	
 	init();
 
 	while(1)
@@ -32,16 +29,8 @@ void main(void)
 				output_pulse[CHANNEL2] = input_pulse[CHANNEL2];
 				output_pulse[CHANNEL3] = input_pulse[CHANNEL3];
 				output_pulse[CHANNEL4] = input_pulse[CHANNEL4];
-	
 				
 				startPPM(10,BEGIN);
-			}
-			debug_channel = 0;
-			if (SWITCH1) {
-				debug_channel |= 1;
-			}
-			if (SWITCH2) {
-				debug_channel |= 2;
 			}
 		}
 	}
