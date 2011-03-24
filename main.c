fc4ef7ac12cc8dac5f2b3b05ee73a2406d52d277
Adly Abdullah <slebetman@yahoo.com>
2011-03-24 18:32:52 +0800

    Cleaned up main.c of test code

--- main.c
+++ main.c
@@ -14,7 +14,6 @@ unsigned char tick; // timer tick (roughly 1ms using 24 MHz XTAL)
 
 void main(void)
 {
-	unsigned int flap_position = 0, temp;
 	init();
 
 	while(1)
@@ -26,40 +25,10 @@ void main(void)
 			if (input_done) {
 				input_done = 0;
 
-				temp = AILERON;				
-				if (SWITCH4) {
-					AILERON = expo(AILERON, 65);
-				}
-				
-				if (SWITCH1) { // ===================================== ELEVON
-					output_pulse[CHANNEL3] = THROTTLE;
-					output_pulse[CHANNEL4] = RUDDER;
-					mix(AILERON, ELEVATOR,
-						&output_pulse[CHANNEL1], &output_pulse[CHANNEL2]);
-				}
-				else if (SWITCH2) { // ================================ FLAPPERON
-					output_pulse[CHANNEL4] = ELEVATOR;
-					output_pulse[CHANNEL3] = THROTTLE;
-					if (DIGITAL1) {
-						slowdown(2500, &flap_position, 70);
-					}
-					else {
-						slowdown(0, &flap_position, -70);
-					}
-					mix(AILERON, flap_position+SERVO_CENTER,
-						&output_pulse[CHANNEL1], &output_pulse[CHANNEL2]);
-				}
-				else if (SWITCH3) { // ================================ SAILBOAT
-					output_pulse[CHANNEL1] = AILERON;
-					output_pulse[CHANNEL3] = scale(THROTTLE, 170)+400;
-				}
-				else { // ============================================= NORMAL
-					output_pulse[CHANNEL1] = AILERON;
-					output_pulse[CHANNEL2] = ELEVATOR;
-					output_pulse[CHANNEL3] = THROTTLE;
-					output_pulse[CHANNEL4] = RUDDER;
-				}
-				AILERON = temp; // restore AILERON in case expo is on
+				output_pulse[CHANNEL1] = AILERON;
+				output_pulse[CHANNEL2] = ELEVATOR;
+				output_pulse[CHANNEL3] = THROTTLE;
+				output_pulse[CHANNEL4] = RUDDER;
 				
 				startPPM(10, BEGIN);
 			}
