72d599690674fdc31d356b18fbac67c80eed6c3e
Adly Abdullah <slebetman@yahoo.com>
2011-03-24 10:19:29 +0800

    Fixed build errors related to last edit

--- common.h
+++ common.h
@@ -1,32 +1,12 @@
 /* Assumes PIC16F690 running at 24MHz */
 
-#include pinout.h
+#include "pinout.h"
 
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
 #define enableInterrupts() GIE=1
 #define disableInterrupts() GIE=0
-
-#define SERVO_MIN 6256
-#define SERVO_MAX 11620
-#define SERVO_CENTER ((SERVO_MIN+SERVO_MAX)/2)
-#define SERVO_RANGE (SERVO_MAX-SERVO_MIN)
-
-#define TOTAL_OUTPUT_CHANNELS 6
-#define TOTAL_INPUT_CHANNELS 5
-
-#define CHANNEL1 0
-#define CHANNEL2 1
-#define CHANNEL3 2
-#define CHANNEL4 3
-#define CHANNEL5 4
-#define CHANNEL6 5
-
-#define AILERON  CHANNEL1
-#define ELEVATOR CHANNEL2
-#define THROTTLE CHANNEL3
-#define RUDDER   CHANNEL4
 
 extern unsigned char tick;
 #define TICK_1MS 70
--- config.h
+++ config.h
@@ -1,5 +1,8 @@
 /* Fuse configuration for PIC16F690 */
 
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
 __CONFIG(
 	HS &           // use high speed crystal 
 	UNPROTECT &    // disable code protect
@@ -8,3 +11,5 @@ __CONFIG(
 	MCLRDIS &      // disable MCLR
 	FCMDIS         // disable failsafe clock monitor
 );
+
+#endif
