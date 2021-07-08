2ede161e1e7908f57e0564dd9bc75be498375907
slebetman <slebetman@gmail.com>
2021-07-08 14:25:12 +0800

    Added splash screen

--- firmware/main.c
+++ firmware/main.c
@@ -66,6 +66,13 @@ void main(void)
 	// initTrim();
 	initGUI();
 
+	for (unsigned short delay = 500;delay;delay--) {
+		oled_goto(37,2);
+		oled_write_string("Simple TX");
+		oled_goto(54,4);
+		oled_write_string("v1.0");
+	}
+	
 	modelID = getModelFromEeprom(SAVED_MODEL);
 
 	while(1)
