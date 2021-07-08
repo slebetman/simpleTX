f3c947c81f471018d4fab1c941ada639406a3ca3
slebetman <slebetman@gmail.com>
2021-07-09 04:37:20 +0800

    Fix deadband calculation

--- firmware/drivers/joystick.c
+++ firmware/drivers/joystick.c
@@ -13,7 +13,8 @@ short read_stick(unsigned char channel) {
 	tmp = analog_get(channel);
 	comp = stick_values[channel] - tmp;
 	if (comp > BAND_FILTER || comp < -BAND_FILTER) {
-		if ((tmp - center[channel]) < DEADBAND && (tmp - center[0]) > -DEADBAND) {
+		comp = tmp - center[channel];
+		if (comp < DEADBAND && comp > -DEADBAND) {
 			tmp = center[channel];
 		}
 		stick_values[channel] = tmp;
@@ -23,6 +24,7 @@ short read_stick(unsigned char channel) {
 }
 
 #define CENTER_SAMPLES 16
+#define CENTER_SAMPLES_DIV 4
 
 void init_joystick () {
 	unsigned char i;
@@ -41,6 +43,6 @@ void init_joystick () {
 	}
 
 	for (i=0; i<TOTAL_ANALOG_CHANNELS; i++) {
-		center[i] = center[i] / CENTER_SAMPLES;
+		center[i] = center[i] >> CENTER_SAMPLES_DIV; // divide by 16
 	}
 }
